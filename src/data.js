ceef208ba6d16eac03acc85d7639f3e4650ee794
udayongraph <uday.ongraph@gmail.com>
2022-02-03 17:18:03 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems=[
+    {
+        id:1,
+        img:"https://images.pexels.com/photos/428338/pexels-photo-428338.jpeg?cs=srgb&dl=pexels-spencer-selover-428338.jpg&fm=jpg",
+        title:"WINTER SALE",
+        desc:"PLEASE SHOP ON MY WEBSITE FOR BEST AND SELECTED STOCKS",
+        bg:"f5fafd",
+    },
+    {
+        id:2,
+        img:"https://images.pexels.com/photos/1193942/pexels-photo-1193942.jpeg?cs=srgb&dl=pexels-samarth-singhai-1193942.jpg&fm=jpg",
+        title:"FESTIVAL SALE",
+        desc:"PLEASE SHOP ON MY WEBSITE FOR BEST AND SELECTED STOCKS",
+        bg:"fcf1ed",
+    },
+    {
+        id:3,
+        img:"https://images.pexels.com/photos/1187954/pexels-photo-1187954.jpeg?cs=srgb&dl=pexels-evg-kowalievska-1187954.jpg&fm=jpg",
+        title:"OFF-SEASON SALE",
+        desc:"PLEASE SHOP ON MY WEBSITE FOR BEST AND SELECTED STOCKS",
+        bg:"fbf0f4",
+    }
+]
