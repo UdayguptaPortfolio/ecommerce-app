4ff9766d666946d3bfa3713f9eaada660b3c626e
udayongraph <uday.ongraph@gmail.com>
2022-02-04 20:00:19 +0530

    Frontend Completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,20 @@ import Announcement from "../components/Announcement";
 import React from "react";
 import Navbar from "../components/Navbar";
 import Slider from '../components/Slider';
-
+import Categories from "../components/Categories";
+import Products from "../components/Products";
+import Newsletter from "../components/NewsLetter";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return <div>
       <Announcement/>
       <Navbar/>
       <Slider/>
+      <Categories/>
+      <Products/>
+      <Newsletter/>
+      <Footer/>
   </div>;
 };
 
